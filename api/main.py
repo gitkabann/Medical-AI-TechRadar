7dedc2e28edfb4a199864d73d767bdfa46c52ded
baoyunkai@naton.cn <WOshiwoniu1016>
2025-11-14 16:41:51 +0800

    增加异常错误处理

--- api/main.py
+++ api/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from app.core.config import settings
-
+from app.core.error_handler import app_exception_handler
 app = FastAPI(title=settings.app_name, version=settings.app_version)
-
+# 注册全局异常处理
+app.add_exception_handler(Exception, app_exception_handler)
 @app.get("/health")
 def health():
     return {
